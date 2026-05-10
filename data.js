2f1196d7bccf990f7c747f9c8e918db22e7d3467
Renato Silva <dominguesrds@fab.mil.br>
2026-05-09 22:07:35 -0300

    Initial commit for recanto-bolos

---
+++ data.js
@@ -0,0 +1,54 @@
+let storeData = {
+    name: "Santo Bolo",
+    phone: "5512991885428", // Número do WhatsApp
+    address: "Recanto Formoso, S/N",
+    hours: "Seg a Dom: 07:00 às 20:00",
+    openingTime: "07:00",
+    closingTime: "20:00",
+    deliveryFee: 5.00
+};
+
+let categories = [];
+
+let products = [
+    {
+        id: 1,
+        category: "bolos",
+        name: "Cenoura com cobertura de chocolate",
+        price: 23.00,
+        desc: "Bolo fofinho de cenoura com cobertura cremosa de chocolate.",
+        image: "imgs/cenoura.webp"
+    },
+    {
+        id: 2,
+        category: "bolos",
+        name: "Limão com cobertura de limão",
+        price: 23.00,
+        desc: "Bolo cítrico e refrescante com cobertura deliciosa de limão.",
+        image: "imgs/limao.webp"
+    },
+    {
+        id: 3,
+        category: "bolos",
+        name: "Fubá com cobertura de goiabada",
+        price: 23.00,
+        desc: "O clássico bolo de fubá de vó, com deliciosa cobertura de goiabada derretida.",
+        image: "imgs/fuba.webp"
+    },
+    {
+        id: 5,
+        category: "bolos",
+        name: "Maçã com canela",
+        price: 23.00,
+        desc: "Bolo artesanal de maçã com um toque super especial de canela.",
+        image: "imgs/maca-canela.webp"
+    },
+    {
+        id: 7,
+        category: "bolos",
+        name: "Churros",
+        price: 23.00,
+        desc: "Bolo sabor churros com canela e doce de leite.",
+        image: "imgs/churros.webp"
+    }
+];
